4af9615f25fcd8ca48df0d6a52bd17ab710c2fec
Slackzone Development <develslack@gmail.com>
2026-01-16 06:36:14 -0300

    commit #3 [ realizando arreglos al archivo git_push.sh ] - [ 16-01-2026 ]

--- git_push.sh
+++ git_push.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 clear
+
+fecha=`date +%d-%m-%Y`
+
 echo "========================================================================"
-echo "Por favor ingrese el texto para el commit que va a realizar: "
+echo "Ultimo commit realizado..."
+git log -1
+
+echo "========================================================================"
+echo "Ingrese el nro de commit a realizar: "
+read commit_number
+echo "========================================================================"
+echo "Ahora ingrese el texto para el commit NRO: $commit_number : "
 read commit
 echo "========================================================================"
 
 git add .
 git branch -M main
-git commit -m "$commit"
+git commit -m "commit #$commit_number [ $commit ] - [ $fecha ]"
 git push -u origin main
